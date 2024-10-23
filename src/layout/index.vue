083da420412165d9767c34382fc5a96b3f928075
junying.zhou <junying.zhou@ly.com>
2024-10-23 16:46:34 +0800

    样式待修改

---
+++ src/layout/index.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <div class="layout-container">
+        layout
+    </div>
+</template>
+<script setup lang="ts">
+</script>
+
+
+<style scoped lang="scss">
+.layout-container {
+    width: 100%;
+    height: 100vh;
+    background-color: #f0f2f5;
+}
+</style>
